2f7a18f72ee829aba0864487cd676cfc3441c7c7
sunny-utility-4827 <mzakzook@me.com>
2019-09-05 18:03:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,8 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     language = programmer_hash[:dennis_ritchie][:languages]
-     language.to_s
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
